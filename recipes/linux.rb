cd84956bee6f9f34ef75f7390edca257b51b8dd8
Doug Ireton <doug.ireton@nordstrom.com>
2013-03-28 22:20:22 -0700

    Minor cleanup for linux recipe

--- recipes/linux.rb
+++ recipes/linux.rb
@@ -11,8 +11,6 @@ nexus = data_bag_item('shared', 'nexus')
 chef_version = node['chef-client-upgrade']['version']
 distro = "el5"
 arch = "#{node[:kernel][:machine]}"
-chef_file = "chef-#{chef_version}.#{distro}.#{arch}.rpm"
-nexus_root = "http://mvnrepo.nordstrom.net/nexus/content/repositories/thirdparty/RPM/chef/"
 
 checksum = node['chef-client-upgrade']['rpm_checksum']
 
@@ -26,9 +24,8 @@ remote_file "#{Chef::Config[:file_cache_path]}/#{chef_file}" do
   checksum checksum
 end
 
-package "chef-client" do
+rpm_package "chef-client" do
   action :install
   source "#{Chef::Config[:file_cache_path]}/#{chef_file}"
-  provider Chef::Provider::Package::Rpm
 end
 
