da691926cfb66e83f5a6376f4efb709afeec67aa
Jack Howland <jack.d.howland@nordstrom.com>
2013-05-02 11:57:16 -0700

    changed windows require to windows recommend in metadata

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "jon.decamp@nordstrom.com"
 description      "upgrades the Chef client"
 license          "All rights reserved"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.0"
+version          "0.3.1"
 
-depends "windows", "1.7.0"
+recommends "windows", "1.7.0"
