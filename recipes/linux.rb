830f85e94c4fa85585d05f2478ccb0d38873b066
markgibbons <mark.gibbons@nordstrom.com>
2015-08-19 16:06:36 -0700

    UIS-1419: Install chef-client 12.4.1

--- recipes/linux.rb
+++ recipes/linux.rb
@@ -10,5 +10,6 @@
 yum_package node['unix_chef_client_upgrade']['pkg_name'] do
   action :install
   version node['unix_chef_client_upgrade']['chef_pkg']
+  ignore_failure true
   notifies :create, 'ruby_block[chef-client-upgraded]', :immediately
 end
